b31754eb9741b3e4cbaa82e09961d8c31c85a25e
MTaurus <97911761+mau1878@users.noreply.github.com>
2024-10-11 23:25:59 -0300

    Update inflacion.py

--- inflacion.py
+++ inflacion.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 import plotly.graph_objs as go
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import re
 
@@ -158,7 +158,7 @@ else:
   )
   end_date = st.date_input(
       'Selecciona la fecha de fin:',
-      min_value=daily_cpi.index.min().date(),
+      min_value=start_date,
       max_value=daily_cpi.index.max().date(),
       value=daily_cpi.index.max().date(),
       key='end_date_end_date_input'
@@ -207,7 +207,7 @@ plot_start_date = st.date_input(
   'Selecciona la fecha de inicio para los datos mostrados en el gráfico:',
   min_value=daily_cpi.index.min().date(),
   max_value=daily_cpi.index.max().date(),
-  value=daily_cpi.index.min().date(),
+  value=(daily_cpi.index.max() - timedelta(days=365)).date(),  # Por defecto, 1 año atrás
   key='plot_start_date_input'
 )
 
@@ -215,7 +215,8 @@ plot_start_date = st.date_input(
 show_percentage = st.checkbox('Mostrar valores ajustados por inflación como porcentajes', value=False)
 
 # Diccionario para almacenar los datos de acciones procesados
-stock_data_dict = {}
+stock_data_dict_nominal = {}
+stock_data_dict_adjusted = {}
 
 if tickers_input:
   tickers = [ticker.strip().upper() for ticker in tickers_input.split(',')]
@@ -224,67 +225,91 @@ if tickers_input:
   # Mapeo de tickers a nombres de variables
   ticker_var_map = {ticker: ticker.replace('.', '_') for ticker in tickers}
 
-  # Descargar y procesar datos nominales (sin ajustar por inflación)
-  combined_nominal_df = pd.DataFrame()
-
-  for ticker in tickers:
+  # Descargar y procesar datos para cada ticker
+  for i, ticker in enumerate(tickers):
       try:
           # Descargar datos históricos de la acción desde la fecha seleccionada
-          stock_data_nominal = yf.download(ticker, start=plot_start_date, end=daily_cpi.index.max().date())
+          stock_data = yf.download(ticker, start=plot_start_date, end=daily_cpi.index.max().date())
 
-          if stock_data_nominal.empty:
+          if stock_data.empty:
               st.error(f"No se encontraron datos para el ticker {ticker}.")
               continue
 
           # Asegurar que el índice sea de tipo datetime
-          stock_data_nominal.index = pd.to_datetime(stock_data_nominal.index)
+          stock_data.index = pd.to_datetime(stock_data.index)
 
           # Ajustar precios por splits
-          stock_data_nominal = ajustar_precios_por_splits(stock_data_nominal, ticker)
+          stock_data = ajustar_precios_por_splits(stock_data, ticker)
 
-          # Almacenar los datos nominales en el DataFrame combinado
-          combined_nominal_df[ticker_var_map[ticker]] = stock_data_nominal['Close']
+          # Unir con los datos de inflación
+          stock_data = stock_data.join(daily_cpi, how='left')
+          # Rellenar hacia adelante cualquier dato de inflación faltante
+          stock_data['Cumulative_Inflation'].ffill(inplace=True)
+          # Eliminar cualquier fila restante con NaN en 'Cumulative_Inflation'
+          stock_data.dropna(subset=['Cumulative_Inflation'], inplace=True)
+
+          # Calcular 'Inflation_Adjusted_Close'
+          stock_data['Inflation_Adjusted_Close'] = stock_data['Close'] * (stock_data['Cumulative_Inflation'].iloc[-1] / stock_data['Cumulative_Inflation'])
+
+          # Almacenar los datos en los diccionarios
+          var_name = ticker_var_map[ticker]
+          stock_data_dict_nominal[var_name] = stock_data['Close']
+          stock_data_dict_adjusted[var_name] = stock_data['Inflation_Adjusted_Close']
+
+          if show_percentage:
+              # Calcular valores ajustados por inflación como porcentajes relativos al valor inicial
+              stock_data['Inflation_Adjusted_Percentage'] = (stock_data['Inflation_Adjusted_Close'] / stock_data['Inflation_Adjusted_Close'].iloc[0] - 1) * 100
+              # Graficar los cambios porcentuales ajustados por inflación
+              fig.add_trace(go.Scatter(
+                  x=stock_data.index,
+                  y=stock_data['Inflation_Adjusted_Percentage'],
+                  mode='lines',
+                  name=f'{ticker} (%)'
+              ))
+
+              # Añadir una línea horizontal roja en 0%
+              fig.add_shape(
+                  type="line",
+                  x0=stock_data.index.min(),
+                  x1=stock_data.index.max(),
+                  y0=0,
+                  y1=0,
+                  line=dict(color="red", width=2, dash="dash"),
+                  xref="x",
+                  yref="y"
+              )
+          else:
+              # Graficar los precios ajustados por inflación
+              fig.add_trace(go.Scatter(
+                  x=stock_data.index,
+                  y=stock_data['Inflation_Adjusted_Close'],
+                  mode='lines',
+                  name=f'{ticker}'
+              ))
+
+              # Graficar el precio promedio como una línea punteada
+              avg_price = stock_data['Inflation_Adjusted_Close'].mean()
+              fig.add_trace(go.Scatter(
+                  x=stock_data.index,
+                  y=[avg_price] * len(stock_data),
+                  mode='lines',
+                  name=f'{ticker} Precio Promedio',
+                  line=dict(dash='dot')
+              ))
+
+          # Graficar el SMA para el primer ticker solamente
+          if i == 0:
+              stock_data['SMA'] = stock_data['Inflation_Adjusted_Close'].rolling(window=sma_period).mean()
+              fig.add_trace(go.Scatter(
+                  x=stock_data.index,
+                  y=stock_data['SMA'],
+                  mode='lines',
+                  name=f'{ticker} SMA de {sma_period} Periodos',
+                  line=dict(color='orange')
+              ))
 
       except Exception as e:
           st.error(f"Ocurrió un error con el ticker {ticker}: {e}")
-
-  # Rellenar datos faltantes
-  combined_nominal_df.ffill(inplace=True)
-  combined_nominal_df.bfill(inplace=True)
-
-  # Graficar precios nominales
-  for i, ticker in enumerate(tickers):
-      var_name = ticker_var_map[ticker]
-      stock_data_dict[var_name] = combined_nominal_df[var_name]
-
-      if not show_percentage:
-          fig.add_trace(go.Scatter(
-              x=combined_nominal_df.index,
-              y=combined_nominal_df[var_name],
-              mode='lines',
-              name=f'{ticker} (Nominal)'
-          ))
-
-          # Graficar el precio promedio como una línea punteada
-          avg_price = combined_nominal_df[var_name].mean()
-          fig.add_trace(go.Scatter(
-              x=combined_nominal_df.index,
-              y=[avg_price] * len(combined_nominal_df),
-              mode='lines',
-              name=f'{ticker} Precio Promedio',
-              line=dict(dash='dot')
-          ))
-
-      # Graficar el SMA para el primer ticker solamente
-      if i == 0:
-          combined_nominal_df[f'{var_name}_SMA'] = combined_nominal_df[var_name].rolling(window=sma_period).mean()
-          fig.add_trace(go.Scatter(
-              x=combined_nominal_df.index,
-              y=combined_nominal_df[f'{var_name}_SMA'],
-              mode='lines',
-              name=f'{ticker} SMA de {sma_period} Periodos',
-              line=dict(color='orange')
-          ))
 
   # ------------------------------
   # Cálculos o Ratios Personalizados
@@ -321,6 +346,12 @@ if tickers_input:
               # Evitar reemplazar partes de otros tickers
               pattern = re.escape(ticker)
               transformed_expression = re.sub(rf'\b{pattern}\b', var_name, transformed_expression)
+
+          # Crear un DataFrame combinado con todas las series nominales
+          combined_nominal_df = pd.DataFrame(stock_data_dict_nominal)
+          # Rellenar datos faltantes
+          combined_nominal_df.ffill(inplace=True)
+          combined_nominal_df.bfill(inplace=True)
 
           # Evaluar la expresión usando el DataFrame combinado nominal
           custom_series_nominal = combined_nominal_df.eval(transformed_expression, engine='python')
@@ -367,7 +398,7 @@ if tickers_input:
               ))
 
           # Almacenar la serie ajustada en el diccionario para posibles usos futuros
-          stock_data_dict['Custom_Adjusted'] = custom_series_nominal['Inflation_Adjusted_Custom']
+          stock_data_dict_adjusted['Custom_Adjusted'] = custom_series_nominal['Inflation_Adjusted_Custom']
 
       except Exception as e:
           # Obtener nombres de variables disponibles para asistir en la corrección
@@ -388,9 +419,9 @@ if tickers_input:
 
   # Actualizar el diseño del gráfico
   fig.update_layout(
-      title='Precios Históricos Nominales',
+      title='Precios Históricos Ajustados por Inflación',
       xaxis_title='Fecha',
-      yaxis_title='Precio de Cierre (ARS)' if not show_percentage else 'Variación Porcentual (%)',
+      yaxis_title='Precio de Cierre Ajustado (ARS)' if not show_percentage else 'Variación Porcentual (%)',
       legend=dict(
           orientation="h",
           yanchor="bottom",
@@ -401,10 +432,6 @@ if tickers_input:
   )
 
   st.plotly_chart(fig)
-
-  # ------------------------------
-  # Ajustar el precio ajustado por inflación de las acciones individuais para otras visualizaciones si es necesario
-  # (Puede ser omitido si no se requiere adicionalmente)
 
 # ------------------------------
 # Nueva Sección: Comparación de Volatilidad Ajustada por Inflación y Precio Ajustado por Inflación
@@ -422,7 +449,7 @@ vol_comparison_start_date = st.date_input(
   'Selecciona la fecha de inicio para la comparación:',
   min_value=daily_cpi.index.min().date(),
   max_value=daily_cpi.index.max().date(),
-  value=(daily_cpi.index.max() - pd.Timedelta(days=365)).date(),  # Por defecto, 1 año atrás
+  value=(daily_cpi.index.max() - timedelta(days=365)).date(),  # Por defecto, 1 año atrás
   key='vol_comparison_start_date_input'
 )
 vol_comparison_end_date = st.date_input(
@@ -436,31 +463,31 @@ vol_comparison_end_date = st.date_input(
 if selected_ticker:
   try:
       # Descargar datos históricos nominales para el ticker seleccionado
-      stock_data_nominal = yf.download(selected_ticker, start=vol_comparison_start_date, end=vol_comparison_end_date)
+      stock_data = yf.download(selected_ticker, start=vol_comparison_start_date, end=vol_comparison_end_date)
 
-      if stock_data_nominal.empty:
+      if stock_data.empty:
           st.error(f"No se encontraron datos para el ticker {selected_ticker}.")
       else:
           # Ajustar precios por splits
-          stock_data_nominal = ajustar_precios_por_splits(stock_data_nominal, selected_ticker)
+          stock_data = ajustar_precios_por_splits(stock_data, selected_ticker)
 
           # Unir con los datos de inflación
-          stock_data_nominal = stock_data_nominal.join(daily_cpi, how='left')
+          stock_data = stock_data.join(daily_cpi, how='left')
           # Rellenar hacia adelante cualquier dato de inflación faltante
-          stock_data_nominal['Cumulative_Inflation'].ffill(inplace=True)
+          stock_data['Cumulative_Inflation'].ffill(inplace=True)
           # Eliminar cualquier fila restante con NaN en 'Cumulative_Inflation'
-          stock_data_nominal.dropna(subset=['Cumulative_Inflation'], inplace=True)
+          stock_data.dropna(subset=['Cumulative_Inflation'], inplace=True)
 
           # Calcular Precio Ajustado por Inflación
-          stock_data_nominal['Inflation_Adjusted_Close'] = stock_data_nominal['Close'] * (stock_data_nominal['Cumulative_Inflation'].iloc[-1] / stock_data_nominal['Cumulative_Inflation'])
+          stock_data['Inflation_Adjusted_Close'] = stock_data['Close'] * (stock_data['Cumulative_Inflation'].iloc[-1] / stock_data['Cumulative_Inflation'])
 
           # Calcular Retornos Diarios Nominales y Ajustados por Inflación
-          stock_data_nominal['Return_Nominal'] = stock_data_nominal['Close'].pct_change()
-          stock_data_nominal['Return_Adjusted'] = stock_data_nominal['Inflation_Adjusted_Close'].pct_change()
+          stock_data['Return_Nominal'] = stock_data['Close'].pct_change()
+          stock_data['Return_Adjusted'] = stock_data['Inflation_Adjusted_Close'].pct_change()
 
           # Calcular Volatilidad Histórica (Desviación Estándar de los Retornos)
-          volatility_nominal = stock_data_nominal['Return_Nominal'].std() * (252**0.5)  # Anualizada
-          volatility_adjusted = stock_data_nominal['Return_Adjusted'].std() * (252**0.5)  # Anualizada
+          volatility_nominal = stock_data['Return_Nominal'].std() * (252**0.5)  # Anualizada
+          volatility_adjusted = stock_data['Return_Adjusted'].std() * (252**0.5)  # Anualizada
 
           # Mostrar Volatilidades
           st.write(f"**Volatilidad Histórica Nominal:** {volatility_nominal:.2%}")
@@ -470,23 +497,36 @@ if selected_ticker:
           fig_vol = go.Figure()
 
           fig_vol.add_trace(go.Scatter(
-              x=stock_data_nominal.index,
-              y=stock_data_nominal['Inflation_Adjusted_Close'],
+              x=stock_data.index,
+              y=stock_data['Inflation_Adjusted_Close'],
               mode='lines',
               name=f'{selected_ticker} Precio Ajustado por Inflación'
           ))
 
           # Graficar Volatilidad como una línea horizontal
+          mean_adjusted_price = stock_data['Inflation_Adjusted_Close'].mean()
+          volatility_line = volatility_adjusted * mean_adjusted_price
+
           fig_vol.add_shape(
               type="line",
-              x0=stock_data_nominal.index.min(),
-              x1=stock_data_nominal.index.max(),
-              y0=volatility_adjusted * stock_data_nominal['Inflation_Adjusted_Close'].mean(),
-              y1=volatility_adjusted * stock_data_nominal['Inflation_Adjusted_Close'].mean(),
+              x0=stock_data.index.min(),
+              x1=stock_data.index.max(),
+              y0=volatility_line,
+              y1=volatility_line,
               line=dict(color="green", width=2, dash="dash"),
               xref="x",
               yref="y",
               name="Volatilidad Ajustada"
+          )
+
+          # Añadir una marca de agua al gráfico
+          fig_vol.add_annotation(
+              text="MTaurus - X: mtaurus_ok",
+              xref="paper", yref="paper",
+              x=0.5, y=0.5,
+              showarrow=False,
+              font=dict(size=30, color="rgba(150, 150, 150, 0.3)"),
+              opacity=0.2
           )
 
           # Actualizar diseño del gráfico
