73eef18bb00e983ff3b510bf318369261b6b8170
MTaurus <97911761+mau1878@users.noreply.github.com>
2026-09-22 21:53:43 -0300

    Update inflacion.py

--- inflacion.py
+++ inflacion.py
@@ -392,7 +392,7 @@ splits = {
     'WMT.BA': {'ratio': 3, 'date': datetime(2024, 1, 23)},
     'AGRO.BA': [
         {'ratio': 6, 'date': datetime(2023, 11, 3), 'type': 'divide'},
-        {'ratio': 2.1, 'date': datetime(2023, 11, 3), 'type': 'multiply'},
+        {'ratio': 2.1, 'date': datetime(2023, 11, 7), 'type': 'multiply'},
     ],
     'ECOG.BA': {'ratio': 10, 'date': datetime(2025, 8, 18)},
 }
